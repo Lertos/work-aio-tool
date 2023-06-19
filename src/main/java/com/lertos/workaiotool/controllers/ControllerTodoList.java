f88b4c204e0ee4346a79b5a8feac6221952051f2
Lertos <deetasticgaming@gmail.com>
2023-06-19 19:17:47 -0300

    Cleanup and making sure the rows show properly

--- src/main/java/com/lertos/workaiotool/controllers/ControllerTodoList.java
+++ src/main/java/com/lertos/workaiotool/controllers/ControllerTodoList.java
@@ -3,42 +3,59 @@ package com.lertos.workaiotool.controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.input.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 import java.io.Serializable;
 
 public class ControllerTodoList {
 
-    private static final ObservableList<Category> categories = FXCollections.observableArrayList(
-            new Category("1", "First line"),
-            new Category("2", "Second line"),
-            new Category("3", "Third line"),
-            new Category("4", "Fourth line")
-    );
-    private final DataFormat CATEGORY_DATA_FORMAT = new DataFormat("lertos-category");
     @FXML
     private VBox vboxTodoList;
 
+    private static final ObservableList<TodoItem> todoItems = FXCollections.observableArrayList(
+            new TodoItem("1", "First line"),
+            new TodoItem("2", "Second line"),
+            new TodoItem("3", "Third line"),
+            new TodoItem("4", "Fourth line")
+    );
+    private final DataFormat TODO_ITEM_DATA_FORMAT = new DataFormat("format-todo-item");
+
     @FXML
     public void initialize() {
-        ListView<Category> categoryListView = new ListView<>(categories);
-        categoryListView.setCellFactory(param -> new CategoryCell());
+        ListView<TodoItem> categoryListView = new ListView<>(todoItems);
+        categoryListView.setCellFactory(param -> new TodoItemCell());
 
         vboxTodoList.getChildren().add(categoryListView);
     }
 
-    public record Category(
+    public record TodoItem(
             String name,
             String description
 
     ) implements Serializable {
     }
 
-    private final class CategoryCell extends ListCell<Category> {
-        public CategoryCell() {
+    private final class TodoItemCell extends ListCell<TodoItem> {
+        HBox hbox = new HBox();
+        Label label = new Label();
+        Pane pane = new Pane();
+        Button button = new Button();
+
+        public TodoItemCell() {
+            super();
+
+            hbox.getChildren().addAll(label, pane, button);
+            hbox.setHgrow(pane, Priority.ALWAYS); //Puts the label to the left and grows the pane so the button(s) will be push to the far right
+
+            button.setOnAction(event -> System.out.println(getItem().name + " : " + event));
 
             setOnDragDetected(event -> {
                 if (getItem() == null)
@@ -48,7 +65,7 @@ public class ControllerTodoList {
                 ClipboardContent content = new ClipboardContent();
 
                 content.putString(getItem().toString());
-                content.put(CATEGORY_DATA_FORMAT, getItem());
+                content.put(TODO_ITEM_DATA_FORMAT, getItem());
 
                 dragboard.setContent(content);
 
@@ -56,20 +73,20 @@ public class ControllerTodoList {
             });
 
             setOnDragOver(event -> {
-                if (event.getGestureSource() != this && event.getDragboard().hasContent(CATEGORY_DATA_FORMAT)) {
+                if (event.getGestureSource() != this && event.getDragboard().hasContent(TODO_ITEM_DATA_FORMAT)) {
                     event.acceptTransferModes(TransferMode.MOVE);
                 }
                 event.consume();
             });
 
             setOnDragEntered(event -> {
-                if (event.getGestureSource() != this && event.getDragboard().hasContent(CATEGORY_DATA_FORMAT)) {
+                if (event.getGestureSource() != this && event.getDragboard().hasContent(TODO_ITEM_DATA_FORMAT)) {
                     setOpacity(0.3);
                 }
             });
 
             setOnDragExited(event -> {
-                if (event.getGestureSource() != this && event.getDragboard().hasContent(CATEGORY_DATA_FORMAT)) {
+                if (event.getGestureSource() != this && event.getDragboard().hasContent(TODO_ITEM_DATA_FORMAT)) {
                     setOpacity(1);
                 }
             });
@@ -81,15 +98,15 @@ public class ControllerTodoList {
                 Dragboard db = event.getDragboard();
                 boolean success = false;
 
-                if (db.hasContent(CATEGORY_DATA_FORMAT)) {
-                    Category droppedCategory = (Category) db.getContent(CATEGORY_DATA_FORMAT);
-                    ObservableList<Category> items = getListView().getItems();
-                    int draggedIdx = items.indexOf(droppedCategory);
+                if (db.hasContent(TODO_ITEM_DATA_FORMAT)) {
+                    TodoItem droppedTodoItem = (TodoItem) db.getContent(TODO_ITEM_DATA_FORMAT);
+                    ObservableList<TodoItem> items = getListView().getItems();
+                    int draggedIdx = items.indexOf(droppedTodoItem);
                     int thisIdx = items.indexOf(getItem());
 
-                    Category temp = categories.get(draggedIdx);
-                    categories.set(draggedIdx, categories.get(thisIdx));
-                    categories.set(thisIdx, temp);
+                    TodoItem temp = todoItems.get(draggedIdx);
+                    todoItems.set(draggedIdx, todoItems.get(thisIdx));
+                    todoItems.set(thisIdx, temp);
 
                     success = true;
                 }
@@ -101,10 +118,18 @@ public class ControllerTodoList {
         }
 
         @Override
-        protected void updateItem(Category item, boolean empty) {
+        protected void updateItem(TodoItem item, boolean empty) {
             super.updateItem(item, empty);
 
-            setText((empty || item == null) ? null : item.name + "\n" + item.description);
+            setText(null);
+
+            if (empty) {
+                setGraphic(null);
+            } else {
+                label.setText(item != null ? item.name + "\n" + item.description : "");
+                //This will make sure the row is shown as expected, with each element in the right order horizontally
+                setGraphic(hbox);
+            }
         }
     }
 }
