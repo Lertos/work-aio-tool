0508f3e7c34f26bce3efdff53a4ad357fb08ae8f
Lertos <deetasticgaming@gmail.com>
2023-06-20 18:35:15 -0300

    Refactoring; changing the overall usage of the dragging events. Changing how the rows are shown

--- src/main/java/com/lertos/workaiotool/controllers/ControllerTodoList.java
+++ src/main/java/com/lertos/workaiotool/controllers/ControllerTodoList.java
@@ -1,19 +1,21 @@
 package com.lertos.workaiotool.controllers;
 
+import com.lertos.workaiotool.model.TodoItem;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
-import java.io.Serializable;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class ControllerTodoList {
 
@@ -21,41 +23,95 @@ public class ControllerTodoList {
     private VBox vboxTodoList;
 
     private static final ObservableList<TodoItem> todoItems = FXCollections.observableArrayList(
-            new TodoItem("1", "First line"),
-            new TodoItem("2", "Second line"),
-            new TodoItem("3", "Third line"),
-            new TodoItem("4", "Fourth line")
+            new TodoItem(false, "First line"),
+            new TodoItem(false, "Second line"),
+            new TodoItem(false, "Third line"),
+            new TodoItem(false, "Fourth line")
     );
     private final DataFormat TODO_ITEM_DATA_FORMAT = new DataFormat("format-todo-item");
+    private final double SPACING_BUFFER = 20.0;
+    private final double BUTTON_ICON_SIZE = 18.0;
+    private final double BUTTON_PADDING_SIZE = 4.0;
+    private final String DELETE_BUTTON_PATH = "./src/main/resources/images/delete.png";
 
     @FXML
     public void initialize() {
-        ListView<TodoItem> categoryListView = new ListView<>(todoItems);
-        categoryListView.setCellFactory(param -> new TodoItemCell());
+        ListView<TodoItem> itemsListView = new ListView<>(todoItems);
 
-        vboxTodoList.getChildren().add(categoryListView);
+        itemsListView.setCellFactory(param -> {
+            try {
+                return new TodoItemCell() {
+                    @Override
+                    protected void updateItem(TodoItem item, boolean empty) {
+                        super.updateItem(item, empty);
+
+                        setText(null);
+
+                        if (empty) {
+                            setGraphic(null);
+                        } else {
+                            //Make sure the HBox never gets squishes off the screen
+                            hbox.setPrefWidth(param.getWidth() - SPACING_BUFFER);
+                            hbox.setMaxWidth(param.getWidth() - SPACING_BUFFER);
+
+                            //Make the label wrap the text so the entire text can be seen
+                            label.setWrapText(true);
+                            label.setText(item != null ? item.getDescription() : "");
+
+                            //Set the button's image to the same size of the button, preserving the aspect ratio
+                            double totalButtonSize = BUTTON_ICON_SIZE + BUTTON_PADDING_SIZE;
+
+                            button.setMinSize(totalButtonSize, totalButtonSize);
+                            button.setMaxSize(totalButtonSize, totalButtonSize);
+
+                            imageView.setPreserveRatio(true);
+                            imageView.setFitWidth(BUTTON_ICON_SIZE);
+                            imageView.setFitHeight(BUTTON_ICON_SIZE);
+
+                            button.setGraphic(imageView);
+
+                            //This will make sure the row is shown as expected, with each element in the right order horizontally
+                            setGraphic(hbox);
+                        }
+                    }
+                };
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        vboxTodoList.getChildren().add(0, itemsListView);
+
+        //Refresh the list so the new cell dimensions are updated from the "current getWidth()" call in the updateItem method
+        vboxTodoList.widthProperty().addListener((obs, oldVal, newVal) -> {
+            itemsListView.refresh();
+        });
     }
 
-    public record TodoItem(
-            String name,
-            String description
-
-    ) implements Serializable {
-    }
-
-    private final class TodoItemCell extends ListCell<TodoItem> {
+    private class TodoItemCell extends ListCell<TodoItem> {
         HBox hbox = new HBox();
+        CheckBox checkBox = new CheckBox();
         Label label = new Label();
         Pane pane = new Pane();
+        ImageView imageView = new ImageView(new Image(new FileInputStream(DELETE_BUTTON_PATH)));
         Button button = new Button();
 
-        public TodoItemCell() {
+        public TodoItemCell() throws FileNotFoundException {
             super();
 
-            hbox.getChildren().addAll(label, pane, button);
+            hbox.getChildren().addAll(checkBox, label, pane, button);
+            hbox.setSpacing(4);
             hbox.setHgrow(pane, Priority.ALWAYS); //Puts the label to the left and grows the pane so the button(s) will be push to the far right
 
-            button.setOnAction(event -> System.out.println(getItem().name + " : " + event));
+            button.setOnAction(event -> System.out.println(getItem().getDescription() + " : button clicked: " + event));
+
+            checkBox.setOnAction(event -> {
+                System.out.println(getItem().getDescription() + " : checkbox clicked: " + event);
+                if (getItem().isDone())
+                    label.setTextFill(Color.BLACK);
+                else
+                    label.setTextFill(Color.DARKGRAY);
+                getItem().setDone(!getItem().isDone());
+            });
 
             setOnDragDetected(event -> {
                 if (getItem() == null)
@@ -113,23 +169,7 @@ public class ControllerTodoList {
                 event.setDropCompleted(success);
                 event.consume();
             });
-
             setOnDragDone(DragEvent::consume);
-        }
-
-        @Override
-        protected void updateItem(TodoItem item, boolean empty) {
-            super.updateItem(item, empty);
-
-            setText(null);
-
-            if (empty) {
-                setGraphic(null);
-            } else {
-                label.setText(item != null ? item.name + "\n" + item.description : "");
-                //This will make sure the row is shown as expected, with each element in the right order horizontally
-                setGraphic(hbox);
-            }
         }
     }
 }
