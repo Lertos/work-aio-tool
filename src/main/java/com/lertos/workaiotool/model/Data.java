aee3a87adbed637afdc35ba7ba225af903c921bb
Lertos <deetasticgaming@gmail.com>
2023-06-20 19:11:29 -0300

    Setting up singleton class for data

--- src/main/java/com/lertos/workaiotool/model/Data.java
+++ src/main/java/com/lertos/workaiotool/model/Data.java
@@ -1,4 +1,15 @@
 package com.lertos.workaiotool.model;
 
 public class Data {
+
+    private static Data instance;
+
+    private Data() {
+    }
+
+    public static Data getInstance() {
+        if (instance == null)
+            instance = new Data();
+        return instance;
+    }
 }
