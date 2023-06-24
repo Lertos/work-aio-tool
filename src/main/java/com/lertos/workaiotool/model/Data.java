c20cf1cdda1095f5218ed4868db4ef5ad1491d08
Lertos <deetasticgaming@gmail.com>
2023-06-23 21:21:47 -0300

    Adding two new lists and getters; getting rid of unneeded constant

--- src/main/java/com/lertos/workaiotool/model/Data.java
+++ src/main/java/com/lertos/workaiotool/model/Data.java
@@ -2,14 +2,18 @@ package com.lertos.workaiotool.model;
 
 import javafx.scene.paint.Color;
 
+import java.util.List;
+
 public class Data {
 
     private static Data instance;
 
+    private List<TodoItem> activeTodoItems;
+    private List<TodoItem> historyTodoItems;
+
     public final Color TEXT_ACTIVE = Color.BLACK;
     public final Color TEXT_INACTIVE = Color.DARKGRAY;
     public final Color TEXT_ERROR = Color.RED;
-    public final Color TEXT_WARNING = Color.YELLOW;
     public final String DELETE_BUTTON_PATH = "./src/main/resources/images/delete.png";
 
     private Data() {
@@ -19,5 +23,13 @@ public class Data {
         if (instance == null)
             instance = new Data();
         return instance;
+    }
+
+    public List<TodoItem> getActiveTodoItems() {
+        return activeTodoItems;
+    }
+
+    public List<TodoItem> getHistoryTodoItems() {
+        return historyTodoItems;
     }
 }
