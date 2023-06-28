2a1bc1a61f3831a7de8915075003f89e14935e2d
Lertos <deetasticgaming@gmail.com>
2023-06-27 21:39:49 -0300

    Adding a helper method to allow a control to have custom "tab" logic

--- src/main/java/com/lertos/workaiotool/Helper.java
+++ src/main/java/com/lertos/workaiotool/Helper.java
@@ -1,11 +1,15 @@
 package com.lertos.workaiotool;
 
 import com.lertos.workaiotool.model.Data;
+import javafx.event.EventHandler;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
+import javafx.scene.robot.Robot;
 
 public class Helper {
 
@@ -33,8 +37,31 @@ public class Helper {
         return hBox;
     }
 
+    //Displays a simple alert with the given text parameter as a string
     public static void showAlert(String text) {
         new Alert(Alert.AlertType.NONE, text, ButtonType.OK).showAndWait();
+    }
+
+    //To allow a control to consume the tab event and instead use a Robot to navigate to the next control
+    public static EventHandler<KeyEvent> consumeTabEventForControl() {
+        return new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                KeyCode code = event.getCode();
+
+                if (code == KeyCode.TAB && !event.isShiftDown() && !event.isControlDown()) {
+                    event.consume();
+                    try {
+                        Robot robot = new Robot();
+                        robot.keyPress(KeyCode.CONTROL);
+                        robot.keyPress(KeyCode.TAB);
+                        robot.keyRelease(KeyCode.TAB);
+                        robot.keyRelease(KeyCode.CONTROL);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+        };
     }
 
 }
