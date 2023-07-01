78069ad2fc50cf5baee4e287bd95b04e10ce4297
Lertos <deetasticgaming@gmail.com>
2023-07-01 19:40:31 -0300

    Adding new icon path

--- src/main/java/com/lertos/workaiotool/model/Data.java
+++ src/main/java/com/lertos/workaiotool/model/Data.java
@@ -19,6 +19,7 @@ public class Data {
     public final Color TEXT_INACTIVE = Color.DARKGRAY;
     public final Color TEXT_ERROR = Color.RED;
     public final String DELETE_BUTTON_PATH = "./src/main/resources/images/delete.png";
+    public final String EDIT_BUTTON_PATH = "./src/main/resources/images/edit.png";
     //Giving different options for spacing throughout the system to keep the look consistent
     public final double DEFAULT_BUTTON_SPACING = 10;
     public final double DEFAULT_GRID_PANE_SPACING = 10;
