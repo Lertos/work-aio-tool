98ad9795ad9d4730103ed3b0f9757fdcad88546d
Lertos <deetasticgaming@gmail.com>
2023-07-01 20:03:16 -0300

    Adding checkbox to check if the path exists on add/update

--- src/main/java/com/lertos/workaiotool/popups/FolderPopup.java
+++ src/main/java/com/lertos/workaiotool/popups/FolderPopup.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -15,6 +16,8 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class FolderPopup {
 
@@ -39,9 +42,12 @@ public class FolderPopup {
 
         Text txtDisplayName = new Text("Display Text");
         Text txtPathToOpen = new Text("Path to Open");
+        Text txtCheckPathExists = new Text("Check Path Exists");
 
         TextField tfDisplayName = new TextField();
         TextField tfPathToOpen = new TextField();
+
+        CheckBox cbCheckPathExists = new CheckBox();
 
         Button btnCancel = new Button("Cancel");
         Button btnSave;
@@ -67,6 +73,8 @@ public class FolderPopup {
         gridPane.add(tfDisplayName, 1, 0);
         gridPane.add(txtPathToOpen, 0, 1);
         gridPane.add(tfPathToOpen, 1, 1);
+        gridPane.add(txtCheckPathExists, 0, 2);
+        gridPane.add(cbCheckPathExists, 1, 2);
 
         //Set the padding
         layout.setPadding(new Insets(10, 10, 10, 10));
@@ -84,6 +92,9 @@ public class FolderPopup {
         GridPane.setHalignment(txtDisplayName, HPos.RIGHT);
         GridPane.setHalignment(txtPathToOpen, HPos.RIGHT);
 
+        //Set other attributes
+        cbCheckPathExists.setSelected(true); //Default=ON
+
         //Set spacing
         buttonHBox.setSpacing(Data.getInstance().DEFAULT_BUTTON_SPACING);
 
@@ -99,13 +110,23 @@ public class FolderPopup {
 
             //First check the validations
             if (tfDisplayName.getText().trim().isEmpty()) {
-                Helper.showAlert("Display Name cannot be empty");
+                Helper.showAlert("'Display Name' cannot be empty");
                 return;
             }
 
             if (tfPathToOpen.getText().trim().isEmpty()) {
-                Helper.showAlert("Path to Open cannot be empty");
+                Helper.showAlert("'Path to Open' cannot be empty");
                 return;
+            }
+
+            //If the checkbox is checked, check to make sure the path exists before saving
+            if (cbCheckPathExists.isSelected()) {
+                File f = new File(tfPathToOpen.getText().trim());
+
+                if (!f.isDirectory()) {
+                    Helper.showAlert("The given path does not exist");
+                    return;
+                }
             }
 
             //If adding a new item
