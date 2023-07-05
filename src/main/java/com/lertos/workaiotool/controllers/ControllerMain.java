0ba5a3af7162fc8cfd40530d55b00d8f232541ad
Lertos <deetasticgaming@gmail.com>
2023-07-04 21:40:28 -0300

    Adding logic to save the new dimensions of the application on window resize

--- src/main/java/com/lertos/workaiotool/controllers/ControllerMain.java
+++ src/main/java/com/lertos/workaiotool/controllers/ControllerMain.java
@@ -1,5 +1,7 @@
 package com.lertos.workaiotool.controllers;
 
+import com.lertos.workaiotool.model.Config;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
@@ -22,6 +24,10 @@ public class ControllerMain {
     @FXML
     private Tab tabSQLCompare;
 
+    private boolean isSaveTimerRunning = false;
+    private double currentWidth = 0;
+    private double currentHeight = 0;
+
     public void setListeners(Scene scene) {
         scene.setOnKeyPressed(event -> {
             //NOTE: It seems text fields properly grasp the focus and do not filter past it so this works as intended
@@ -34,9 +40,43 @@ public class ControllerMain {
                 case DIGIT6 -> switchTab(tabSQLCompare);
             }
         });
+
+        scene.widthProperty().addListener((obs, oldVal, newVal) -> {
+            saveNewDimensions(scene);
+        });
+
+        scene.heightProperty().addListener((obs, oldVal, newVal) -> {
+            saveNewDimensions(scene);
+        });
+    }
+
+    private void saveNewDimensions(Scene scene) {
+        currentWidth = scene.getWidth();
+        currentHeight = scene.getHeight();
+
+        if (!isSaveTimerRunning)
+            startSaveTimer();
     }
 
     private void switchTab(Tab tab) {
         tabPaneMain.getSelectionModel().select(tab);
+    }
+
+    private void startSaveTimer() {
+        Runnable task = () -> {
+            try {
+                isSaveTimerRunning = true;
+                Thread.sleep(Config.getInstance().SAVE_TIMER_WINDOW);
+
+                Platform.runLater(() -> {
+                    isSaveTimerRunning = false;
+
+                    //TODO: Implement the save
+                    System.out.println("FINAL == " + currentWidth + " " + currentHeight);
+                });
+            } catch (InterruptedException event) {
+            }
+        };
+        new Thread(task).start();
     }
 }
