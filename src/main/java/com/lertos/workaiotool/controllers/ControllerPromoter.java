228706f4a864c81d01b859a818c10ea99cabdd73
Lertos <deetasticgaming@gmail.com>
2023-07-09 10:56:12 -0300

    Removing default values; adding method call to display for testing purposes

--- src/main/java/com/lertos/workaiotool/controllers/ControllerPromoter.java
+++ src/main/java/com/lertos/workaiotool/controllers/ControllerPromoter.java
@@ -1,15 +1,12 @@
 package com.lertos.workaiotool.controllers;
 
+import com.lertos.workaiotool.popups.PromotePopup;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class ControllerPromoter {
 
     @FXML
-    private Label welcomeText;
-
-    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application from " + this.getClass().getSimpleName());
+        PromotePopup.display(0);
     }
 }
