0829ddecdca48ce1534ceaaed37583665b3d6364
Lertos <deetasticgaming@gmail.com>
2023-07-11 19:01:17 -0300

    Adding the InfoItem popup

---
+++ src/main/java/com/lertos/workaiotool/popups/InfoPopup.java
@@ -0,0 +1,140 @@
+package com.lertos.workaiotool.popups;
+
+import com.lertos.workaiotool.Helper;
+import com.lertos.workaiotool.model.Config;
+import com.lertos.workaiotool.model.Data;
+import com.lertos.workaiotool.model.items.InfoItem;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class InfoPopup {
+
+    private static boolean updated = false;
+
+    public static boolean display(int itemIndex) {
+        //Get the InfoItem from the list
+        InfoItem item = null;
+
+        if (itemIndex != -1 && itemIndex < Data.getInstance().infoItems.getActiveItems().size())
+            item = Data.getInstance().infoItems.getActiveItems().get(itemIndex);
+
+        //Create the popup window
+        Stage popupWindow = new Stage();
+
+        popupWindow.initModality(Modality.APPLICATION_MODAL);
+
+        //Create the controls
+        VBox layout = new VBox(Config.getInstance().DEFAULT_CONTROL_SPACING);
+
+        GridPane gridPane = new GridPane();
+
+        Text txtDisplayName = new Text("Display Text");
+        Text txtAdditionalText = new Text("Additional Text");
+
+        TextField tfDisplayName = new TextField();
+        TextArea taAdditionalText = new TextArea();
+
+        Button btnCancel = new Button("Cancel");
+        Button btnSave;
+
+        if (item == null)
+            btnSave = new Button("Add");
+        else
+            btnSave = new Button("Update");
+
+        HBox buttonHBox = new HBox(btnCancel, btnSave);
+
+        //Set the values of the text fields from the item info
+        if (item != null) {
+            tfDisplayName.setText(item.getDescription());
+            taAdditionalText.setText(item.getAdditionalText());
+        }
+
+        //Add children
+        layout.getChildren().addAll(gridPane, buttonHBox);
+
+        //Arranging all the nodes in the grid
+        gridPane.add(txtDisplayName, 0, 0);
+        gridPane.add(tfDisplayName, 1, 0);
+        gridPane.add(txtAdditionalText, 0, 1);
+        gridPane.add(taAdditionalText, 1, 1);
+
+        //Set the padding
+        layout.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        //Set the vertical and horizontal gaps between the columns
+        gridPane.setVgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
+        gridPane.setHgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
+
+        //Set the alignments
+        layout.setAlignment(Pos.CENTER);
+        gridPane.setAlignment(Pos.CENTER);
+        buttonHBox.setAlignment(Pos.CENTER);
+
+        GridPane.setHalignment(txtDisplayName, HPos.RIGHT);
+        GridPane.setHalignment(txtAdditionalText, HPos.RIGHT);
+
+        //Set spacing
+        buttonHBox.setSpacing(Config.getInstance().DEFAULT_BUTTON_SPACING);
+
+        //Set other attributes
+        taAdditionalText.setWrapText(true);
+        taAdditionalText.setPrefRowCount(4);
+        taAdditionalText.setPrefColumnCount(tfDisplayName.getPrefColumnCount() * 2);
+
+        //Adding an event handler so tabs can be used to go to the next control in a text area
+        taAdditionalText.addEventFilter(KeyEvent.KEY_PRESSED, Helper.consumeTabEventForControl());
+
+        //Set listeners
+        btnCancel.setOnAction(e -> {
+            updated = false;
+
+            popupWindow.close();
+        });
+
+        btnSave.setOnAction(e -> {
+            updated = true;
+
+            //First check the validations
+            if (tfDisplayName.getText().trim().isEmpty()) {
+                Helper.showAlert("Display Name cannot be empty");
+                return;
+            }
+
+            //If adding a new item
+            if (itemIndex == -1) {
+                InfoItem newItem = new InfoItem(tfDisplayName.getText().trim(), taAdditionalText.getText().trim());
+
+                Data.getInstance().infoItems.getActiveItems().add(newItem);
+            }
+            //If updating an existing InfoItem
+            else {
+                Data.getInstance().infoItems.getActiveItems().get(itemIndex).setDescription(tfDisplayName.getText().trim());
+                Data.getInstance().infoItems.getActiveItems().get(itemIndex).setAdditionalText(taAdditionalText.getText().trim());
+            }
+
+            popupWindow.close();
+        });
+
+        //Create the new scene and show the popup
+        Scene scene = new Scene(layout);
+
+        popupWindow.setScene(scene);
+        popupWindow.showAndWait();
+
+        return updated;
+    }
+}
