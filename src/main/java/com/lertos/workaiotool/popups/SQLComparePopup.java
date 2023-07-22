2aeb9c49080791f097099ed9fc19efdb5449b4ef
Lertos <deetasticgaming@gmail.com>
2023-07-22 13:14:09 -0300

    Getting top section completed

---
+++ src/main/java/com/lertos/workaiotool/popups/SQLComparePopup.java
@@ -0,0 +1,130 @@
+package com.lertos.workaiotool.popups;
+
+import com.lertos.workaiotool.Helper;
+import com.lertos.workaiotool.model.Config;
+import com.lertos.workaiotool.model.Data;
+import com.lertos.workaiotool.model.items.SQLCompareItem;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+public class SQLComparePopup {
+
+    private static boolean updated = false;
+
+    public static boolean display(int itemIndex) {
+        //Get the item info
+        SQLCompareItem item = null;
+
+        if (itemIndex != -1 && itemIndex < Data.getInstance().sqlCompareItems.getActiveItems().size())
+            item = Data.getInstance().sqlCompareItems.getActiveItems().get(itemIndex);
+
+        //Create the popup window
+        Stage popupWindow = new Stage();
+
+        popupWindow.initModality(Modality.APPLICATION_MODAL);
+
+        //Create the controls
+        VBox layout = new VBox(Config.getInstance().DEFAULT_CONTROL_SPACING);
+
+        //Top section controls
+        GridPane gridPane = new GridPane();
+
+        Text txtDisplayName = new Text("Display Text");
+        Text txtPathToOpen = new Text("SQL Type");
+        Text txtProcedureName = new Text("Procedure Name");
+
+        TextField tfDisplayName = new TextField();
+        TextField tfProcedureName = new TextField();
+
+        //Path Types
+        ToggleGroup groupSQLTypes = new ToggleGroup();
+        VBox vboxSQLTypes = Helper.getEnumRadioButtons(SQLCompareItem.SQLType.values(), groupSQLTypes);
+
+        Button btnCancel = new Button("Cancel");
+        Button btnSave;
+
+        if (item == null)
+            btnSave = new Button("Add");
+        else
+            btnSave = new Button("Update");
+
+        HBox buttonHBox = new HBox(btnCancel, btnSave);
+
+        //Set the values of the text fields from the item info
+        if (item != null) {
+            tfDisplayName.setText(item.getDescription());
+            tfProcedureName.setText(item.getProcedureName());
+        }
+        //Arranging all the nodes in the grid
+        gridPane.add(txtDisplayName, 0, 0);
+        gridPane.add(tfDisplayName, 1, 0);
+        gridPane.add(txtPathToOpen, 0, 1);
+        gridPane.add(vboxSQLTypes, 1, 1);
+        gridPane.add(txtProcedureName, 0, 2);
+        gridPane.add(tfProcedureName, 1, 2);
+
+        //Add children
+        layout.getChildren().addAll(gridPane, buttonHBox);
+
+        //Set the padding
+        layout.setPadding(new Insets(10, 10, 10, 10));
+        gridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        //Set the vertical and horizontal gaps between the columns
+        gridPane.setVgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
+        gridPane.setHgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
+
+        //Set the alignments
+        layout.setAlignment(Pos.CENTER);
+        gridPane.setAlignment(Pos.CENTER);
+        buttonHBox.setAlignment(Pos.CENTER);
+
+        GridPane.setHalignment(txtDisplayName, HPos.RIGHT);
+        GridPane.setHalignment(txtPathToOpen, HPos.RIGHT);
+
+        //Set spacing
+        buttonHBox.setSpacing(Config.getInstance().DEFAULT_BUTTON_SPACING);
+
+        //Set listeners
+        btnCancel.setOnAction(e -> {
+            updated = false;
+
+            popupWindow.close();
+        });
+
+        btnSave.setOnAction(e -> {
+            updated = true;
+
+            //First check the validations
+            if (tfDisplayName.getText().trim().isEmpty()) {
+                Helper.showAlert("'Display Name' cannot be empty");
+                return;
+            } else if (tfProcedureName.getText().trim().isEmpty()) {
+                Helper.showAlert("'Procedure Name' cannot be empty");
+                return;
+            }
+
+            popupWindow.close();
+        });
+
+        //Create the new scene and show the popup
+        Scene scene = new Scene(layout);
+
+        popupWindow.setScene(scene);
+        popupWindow.showAndWait();
+
+        return updated;
+    }
+
+}
