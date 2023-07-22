c38715327e619da6e9c9b3b302ce5abed2088159
Lertos <deetasticgaming@gmail.com>
2023-07-22 15:56:52 -0300

    Adding logic in own methods; setting up tabs for servers and reusable code

--- src/main/java/com/lertos/workaiotool/popups/SQLComparePopup.java
+++ src/main/java/com/lertos/workaiotool/popups/SQLComparePopup.java
@@ -20,6 +20,8 @@ import javafx.stage.Stage;
 public class SQLComparePopup {
 
     private static boolean updated = false;
+    private static TabPane tabPane;
+    private static int prefColCount;
 
     public static boolean display(int itemIndex) {
         //Get the item info
@@ -33,8 +35,12 @@ public class SQLComparePopup {
 
         popupWindow.initModality(Modality.APPLICATION_MODAL);
 
-        //Create the controls
+        //Create the parent to hold all controls
         VBox layout = new VBox(Config.getInstance().DEFAULT_CONTROL_SPACING);
+
+        //Create the tab pane to hold all the server configurations
+        tabPane = new TabPane();
+        tabPane.setPrefSize(350, 300);
 
         //Top section controls
         GridPane gridPane = new GridPane();
@@ -46,10 +52,11 @@ public class SQLComparePopup {
         TextField tfDisplayName = new TextField();
         TextField tfProcedureName = new TextField();
 
-        //Path Types
+        //Set up SQL types
         ToggleGroup groupSQLTypes = new ToggleGroup();
         VBox vboxSQLTypes = Helper.getEnumRadioButtons(SQLCompareItem.SQLType.values(), groupSQLTypes);
 
+        //Set up buttons
         Button btnCancel = new Button("Cancel");
         Button btnSave;
 
@@ -63,8 +70,10 @@ public class SQLComparePopup {
         //Set the values of the text fields from the item info
         if (item != null) {
             tfDisplayName.setText(item.getDescription());
+            //TODO: Set the SQL type
             tfProcedureName.setText(item.getProcedureName());
         }
+
         //Arranging all the nodes in the grid
         gridPane.add(txtDisplayName, 0, 0);
         gridPane.add(tfDisplayName, 1, 0);
@@ -73,39 +82,8 @@ public class SQLComparePopup {
         gridPane.add(txtProcedureName, 0, 2);
         gridPane.add(tfProcedureName, 1, 2);
 
+        //Separator to split the top section from the tab pane with all the server set up
         Separator separator = new Separator(Orientation.HORIZONTAL);
-
-        TabPane tabPane = new TabPane();
-
-        //Set up the dynamic server list
-        GridPane serverGridPane = new GridPane();
-
-        Text txtHost = new Text("Host");
-        Text txtPort = new Text("Port");
-        Text txtUsername = new Text("Username");
-        Text txtPassword = new Text("Password");
-        Text txtDatabases = new Text("Databases");
-
-        TextField tfHost = new TextField();
-        TextField tfPort = new TextField();
-        TextField tfUsername = new TextField();
-        TextField tfPassword = new TextField();
-
-        TextArea taDatabases = new TextArea();
-
-        //Arranging all the nodes in the grid
-        serverGridPane.add(txtHost, 0, 1);
-        serverGridPane.add(tfHost, 1, 1);
-        serverGridPane.add(txtPort, 0, 2);
-        serverGridPane.add(tfPort, 1, 2);
-        serverGridPane.add(txtUsername, 0, 3);
-        serverGridPane.add(tfUsername, 1, 3);
-        serverGridPane.add(txtPassword, 0, 4);
-        serverGridPane.add(tfPassword, 1, 4);
-        serverGridPane.add(txtDatabases, 0, 5);
-        serverGridPane.add(taDatabases, 1, 5);
-
-        tabPane.getTabs().add(new Tab("Test Tab", serverGridPane));
 
         //Add children
         layout.getChildren().addAll(gridPane, separator, createAddNewServerHBox(), tabPane, buttonHBox);
@@ -113,40 +91,30 @@ public class SQLComparePopup {
         //Set the padding
         layout.setPadding(new Insets(10, 10, 10, 10));
         gridPane.setPadding(new Insets(10, 10, 10, 10));
-        serverGridPane.setPadding(new Insets(10, 10, 10, 10));
 
         //Set the vertical and horizontal gaps between the columns
         gridPane.setVgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
         gridPane.setHgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
 
-        serverGridPane.setVgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
-        serverGridPane.setHgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
-
         //Set the alignments
         layout.setAlignment(Pos.CENTER);
         gridPane.setAlignment(Pos.CENTER);
-        serverGridPane.setAlignment(Pos.CENTER);
         buttonHBox.setAlignment(Pos.CENTER);
 
         GridPane.setHalignment(txtDisplayName, HPos.RIGHT);
         GridPane.setHalignment(txtSQLType, HPos.RIGHT);
         GridPane.setHalignment(txtProcedureName, HPos.RIGHT);
-        GridPane.setHalignment(txtHost, HPos.RIGHT);
-        GridPane.setHalignment(txtPort, HPos.RIGHT);
-        GridPane.setHalignment(txtUsername, HPos.RIGHT);
-        GridPane.setHalignment(txtPassword, HPos.RIGHT);
-        GridPane.setHalignment(txtDatabases, HPos.RIGHT);
 
         //Set spacing
         buttonHBox.setSpacing(Config.getInstance().DEFAULT_BUTTON_SPACING);
 
         //Set other attributes
-        int prefColCount = tfDisplayName.getPrefColumnCount() * 2;
-        int prefRowCount = 3;
+        prefColCount = tfDisplayName.getPrefColumnCount() * 2;
 
-        taDatabases.setWrapText(true);
-        taDatabases.setPrefRowCount(prefRowCount);
-        taDatabases.setPrefColumnCount(prefColCount);
+        //Create each tab for each SQLItem
+        if (itemIndex != -1) {
+            //TODO - create tabs
+        }
 
         //Set listeners
         btnCancel.setOnAction(e -> {
@@ -190,16 +158,68 @@ public class SQLComparePopup {
 
         btnAddServer.setOnMouseClicked(mouseEvent -> {
             if (tfTabName.getText().isEmpty()) {
-                Helper.showAlert("'Tab Name' must not be empty=");
+                Helper.showAlert("'Tab Name' must not be empty");
                 return;
             }
 
-            //TODO: Add a new tab - will need to make the TabPane accessible to this method
+            createServerTab(tfTabName.getText());
 
             tfTabName.setText("");
         });
 
         return new HBox(tfTabName, btnAddServer);
+    }
+
+    private static void createServerTab(String tabName) {
+        GridPane serverGridPane = new GridPane();
+
+        Text txtHost = new Text("Host");
+        Text txtPort = new Text("Port");
+        Text txtUsername = new Text("Username");
+        Text txtPassword = new Text("Password");
+        Text txtDatabases = new Text("Databases");
+
+        TextField tfHost = new TextField();
+        TextField tfPort = new TextField();
+        TextField tfUsername = new TextField();
+        TextField tfPassword = new TextField();
+
+        TextArea taDatabases = new TextArea();
+
+        taDatabases.setPromptText("Each line is a new database");
+
+        //Arranging all the nodes in the grid
+        serverGridPane.add(txtHost, 0, 1);
+        serverGridPane.add(tfHost, 1, 1);
+        serverGridPane.add(txtPort, 0, 2);
+        serverGridPane.add(tfPort, 1, 2);
+        serverGridPane.add(txtUsername, 0, 3);
+        serverGridPane.add(tfUsername, 1, 3);
+        serverGridPane.add(txtPassword, 0, 4);
+        serverGridPane.add(tfPassword, 1, 4);
+        serverGridPane.add(txtDatabases, 0, 5);
+        serverGridPane.add(taDatabases, 1, 5);
+
+        //Set the alignments
+        serverGridPane.setAlignment(Pos.CENTER);
+
+        GridPane.setHalignment(txtHost, HPos.RIGHT);
+        GridPane.setHalignment(txtPort, HPos.RIGHT);
+        GridPane.setHalignment(txtUsername, HPos.RIGHT);
+        GridPane.setHalignment(txtPassword, HPos.RIGHT);
+        GridPane.setHalignment(txtDatabases, HPos.RIGHT);
+
+        //Set other attributes
+        serverGridPane.setPadding(new Insets(10, 10, 10, 10));
+
+        serverGridPane.setVgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
+        serverGridPane.setHgap(Config.getInstance().DEFAULT_GRID_PANE_SPACING);
+
+        taDatabases.setWrapText(true);
+        taDatabases.setPrefRowCount(3);
+        taDatabases.setPrefColumnCount(prefColCount);
+
+        tabPane.getTabs().add(new Tab(tabName, serverGridPane));
     }
 
 }
