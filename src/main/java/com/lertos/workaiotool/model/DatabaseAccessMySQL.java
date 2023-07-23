048ac331729fb5df7900c4e439e89d81e6696469
Lertos <deetasticgaming@gmail.com>
2023-07-23 20:37:48 -0300

    Adding validation when building the connection strings

--- src/main/java/com/lertos/workaiotool/model/DatabaseAccessMySQL.java
+++ src/main/java/com/lertos/workaiotool/model/DatabaseAccessMySQL.java
@@ -17,4 +17,21 @@ public class DatabaseAccessMySQL extends DatabaseAccess {
         return "SELECT routine_definition FROM INFORMATION_SCHEMA.ROUTINES WHERE specific_name = '" + procedureName + "'";
     }
 
+    private boolean isValidConnectionString() {
+        if (itemSQL.getHost() == null || itemSQL.getHost().isEmpty()) {
+            Helper.showAlert("The value given for 'host' is empty or cannot be found");
+            return false;
+        } else if (itemSQL.getPort() < 0) {
+            Helper.showAlert("The value given for 'port' is incorrect");
+            return false;
+        } else if (itemSQL.getUsername() == null || itemSQL.getUsername().isEmpty()) {
+            Helper.showAlert("The value given for 'username' is empty or cannot be found");
+            return false;
+        } else if (itemSQL.getPassword() == null || itemSQL.getPassword().isEmpty()) {
+            Helper.showAlert("The value given for 'password' is empty or cannot be found");
+            return false;
+        }
+        return true;
+    }
+
 }
