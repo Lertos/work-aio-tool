ce273e9131ae8362e2247b415aab0aeab48b75df
Lertos <deetasticgaming@gmail.com>
2023-07-23 21:25:17 -0300

    Redoing the logic to be more dynamic after understanding how MySQL works

--- src/main/java/com/lertos/workaiotool/model/DatabaseAccess.java
+++ src/main/java/com/lertos/workaiotool/model/DatabaseAccess.java
@@ -10,8 +10,6 @@ public abstract class DatabaseAccess {
     protected ItemSQL itemSQL;
     protected String procedureName;
     protected String driverString;
-    protected ArrayList<String> connectionStringList;
-    protected String statementString;
     protected String returnedColumnName;
     protected Connection connect = null;
     protected Statement statement = null;
@@ -22,34 +20,58 @@ public abstract class DatabaseAccess {
         this.procedureName = procedureName;
         this.driverString = driverString;
         this.returnedColumnName = returnedColumnName;
-        this.connectionStringList = buildConnectionStrings();
-        this.statementString = buildStatementString();
     }
 
-    protected abstract ArrayList<String> buildConnectionStrings();
+    protected abstract String buildConnectionString(String databaseName);
 
-    protected abstract String buildStatementString();
+    protected abstract String buildStatementString(String databaseName);
 
+    //Returns NULL if definitions do not match, or the definition string if they all match
     public String getProcedureDefinition() {
+        //This will load the driver; each SQL type has its own driver
         try {
-            //This will load the driver; each SQL type has its own driver
             Class.forName(driverString);
-
-            //TODO: Loop through all of the connection strings
-            connect = DriverManager.getConnection(connectionStringList.get(0));
-            statement = connect.createStatement();
-
-            resultSet = statement.executeQuery(statementString);
-
-            return getResult(resultSet);
-        } catch (SQLException sqlException) {
-            Helper.showAlert(sqlException.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            close();
+            Helper.showAlert("Cannot find or load the SQL driver");
         }
-        return null;
+
+        //Iterate over each connection string, get the result, and save it
+        ArrayList<String> results = new ArrayList<>();
+
+        for (String databaseName : itemSQL.getDatabaseNames()) {
+            try {
+                connect = DriverManager.getConnection(buildConnectionString(databaseName));
+                statement = connect.createStatement();
+
+                resultSet = statement.executeQuery(buildStatementString(databaseName));
+
+                results.add(getResult(resultSet));
+            } catch (SQLException sqlException) {
+                Helper.showAlert(sqlException.getMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                close();
+            }
+        }
+
+        //Compare each result and return null if they do not match, or the definition if they do
+        String definitionToCompare = null;
+
+        for (String definition : results) {
+            //Remove all spaces, tabs, and new lines
+            String trimmedDefinition = definition.replace("\n", "").replace("\t", "").replace(" ", "");
+
+            //If this is the first iteration, simply set the definition to compare against
+            if (definitionToCompare == null)
+                definitionToCompare = trimmedDefinition;
+            else {
+                //If the previous definition and the current definition do not match, then we already found our answer
+                if (!trimmedDefinition.equalsIgnoreCase(definitionToCompare))
+                    return null;
+            }
+        }
+        return definitionToCompare;
     }
 
     private String getResult(ResultSet resultSet) throws SQLException {
--- src/main/java/com/lertos/workaiotool/model/DatabaseAccessMySQL.java
+++ src/main/java/com/lertos/workaiotool/model/DatabaseAccessMySQL.java
@@ -2,8 +2,6 @@ package com.lertos.workaiotool.model;
 
 import com.lertos.workaiotool.Helper;
 
-import java.util.ArrayList;
-
 public class DatabaseAccessMySQL extends DatabaseAccess {
 
     public DatabaseAccessMySQL(ItemSQL itemSQL, String procedureName) {
@@ -12,41 +10,35 @@ public class DatabaseAccessMySQL extends DatabaseAccess {
 
     //MYSQL: "jdbc:mysql://<host>/<database>?user=<username>&password=<password>";
     @Override
-    protected ArrayList<String> buildConnectionStrings() {
-        ArrayList<String> connectionStrings = new ArrayList<>();
+    protected String buildConnectionString(String databaseName) {
+        if (!isValidConnectionString())
+            return null;
 
-        if (isValidConnectionString()) {
-            StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
-            //First build the main string and add a wildcard to replace the database name with
-            String databaseWildcard = "<database>";
+        sb.append("jdbc:mysql://");
+        sb.append(itemSQL.getHost());
 
-            sb.append("jdbc:mysql://");
-            sb.append(itemSQL.getHost());
-
-            if (itemSQL.getPort() > -2) {
-                sb.append(":");
-                sb.append(itemSQL.getPort());
-            }
-
-            sb.append("/");
-            sb.append(databaseWildcard);
-            sb.append("?");
-            sb.append("user=");
-            sb.append(itemSQL.getUsername());
-            sb.append("&password=");
-            sb.append(itemSQL.getPassword());
-
-            //Build a new connection string for each database, using the initial string and replacing the wildcard
-            for (String databaseName : itemSQL.getDatabaseNames())
-                connectionStrings.add(sb.toString().replace(databaseWildcard, databaseName));
+        if (itemSQL.getPort() > -2) {
+            sb.append(":");
+            sb.append(itemSQL.getPort());
         }
-        return connectionStrings;
+
+        sb.append("/");
+        sb.append(databaseName);
+        sb.append("?");
+        sb.append("user=");
+        sb.append(itemSQL.getUsername());
+        sb.append("&password=");
+        sb.append(itemSQL.getPassword());
+
+        return sb.toString();
     }
 
+    //MYSQL: SELECT routine_definition FROM INFORMATION_SCHEMA.ROUTINES WHERE specific_name = '<routine_name>' and routine_schema = '<database_name>'
     @Override
-    protected String buildStatementString() {
-        return "SELECT routine_definition FROM INFORMATION_SCHEMA.ROUTINES WHERE specific_name = '" + procedureName + "'";
+    protected String buildStatementString(String databaseName) {
+        return "SELECT routine_definition FROM INFORMATION_SCHEMA.ROUTINES WHERE specific_name = '" + procedureName + "' and routine_schema = '" + databaseName + "'";
     }
 
     private boolean isValidConnectionString() {
