1147eed0fc1c4f7f9879c47506bfcc49f1cd1168
Lertos <deetasticgaming@gmail.com>
2023-07-30 11:49:30 -0300

    Adding checkbox to use integrated security instead

--- src/main/java/com/lertos/workaiotool/model/ItemSQL.java
+++ src/main/java/com/lertos/workaiotool/model/ItemSQL.java
@@ -10,15 +10,17 @@ public class ItemSQL implements Serializable {
     private int port;
     private String username;
     private String password;
+    private boolean useIntegratedSecurity;
     private ArrayList<String> databaseNames;
 
-    public ItemSQL(String tabName, String host, int port, String username, String password, ArrayList<String> databaseNames) {
+    public ItemSQL(String tabName, String host, int port, String username, String password, boolean useIntegratedSecurity, ArrayList<String> databaseNames) {
         this.tabName = tabName;
         this.host = host;
         this.port = port;
         this.databaseNames = databaseNames;
         this.username = username;
         this.password = password;
+        this.useIntegratedSecurity = useIntegratedSecurity;
     }
 
     public String getTabName() {
@@ -43,5 +45,9 @@ public class ItemSQL implements Serializable {
 
     public String getPassword() {
         return password;
+    }
+
+    public boolean usesIntegratedSecurity() {
+        return useIntegratedSecurity;
     }
 }
